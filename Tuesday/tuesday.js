18ac0bc2d51ddaaed5c1981c05ba0dfd010a493a
TeaganVang <tevang@icstars.org>
2025-02-13 15:34:43 -0600

    My JavaScript Start
    Javascript in an HTML document and attaching an external JavaScript file to HTML document.

---
+++ Tuesday/tuesday.js
@@ -0,0 +1,7 @@
+document.body.innerHTML = "This is the ultimate state of human emotion. More passionate than Hope. Far deeper than Despair. It is Love.";
+//Javascript is case sensitive
+var a = 10;
+var b = 35;
+var sum = a + b;
+
+console.log(sum)
